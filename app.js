4a943781cfaf62e46bb8a9e7f0f4b1889e575668
jacobeliks <jacobeliks@gmail.om>
2023-06-24 18:23:53 +0300

    change port

--- app.js
+++ app.js
@@ -57,6 +57,7 @@ app.get('/api/products/:id', async (req, res) => {
   }
 })
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
